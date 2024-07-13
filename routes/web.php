2c643f0aa3195f66996ef17f94dc81e5b83e7837
GerrysatriaH <gerrysatria10@gmail.com>
2024-07-14 01:02:50 +0700

    add locker-layout.blade.php

--- routes/web.php
+++ routes/web.php
@@ -7,9 +7,15 @@ Route::get('/', function () {
     return view('landing-page', ['title' => 'Landing Page']);
 });
 
+// Admin
 Route::get('/admin-dashboard', [AdminController::class, 'dashboard']);
 Route::get('/admin-sign-in', [AdminController::class, 'signIn']);
 Route::get('/admin-daftar-loker', [AdminController::class, 'daftarLoker']);
 Route::get('/admin-daftar-peminjaman', [AdminController::class, 'daftarPeminjaman']);
 Route::get('/admin-riwayat-peminjaman', [AdminController::class, 'riwayatPeminjaman']);
 Route::get('/admin-daftar-ulasan', [AdminController::class, 'daftarUlasan']);
+
+// Locker
+Route::get('/locker', function() {
+    return view('layout/locker-layout', ['title' => 'Peminjaman Loker']);
+});
